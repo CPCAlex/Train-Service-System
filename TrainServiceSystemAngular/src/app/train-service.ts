3d0fabdf16eb1c26440a28577da95ce85c861847
bdbhb6 <bryson.bourisaw@gmail.com>
2024-11-14 14:15:45 -0600

    resulting page implementation

---
+++ TrainServiceSystemAngular/src/app/train-service.ts
@@ -0,0 +1,28 @@
+// train-service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrainService {
+
+  constructor(private http: HttpClient) {}
+
+  getTrainRoute(from: string, to: string, criteria: string, departureTime: string): Observable<any> {
+    const payload = { start_pos: from, destination: to, departure_time: departureTime };
+
+    let apiUrl = '';
+
+    if (criteria === 'cheapest') {
+      apiUrl = 'http://127.0.0.1:5000/api/less_money'; 
+    } else if (criteria === 'quickest') {
+      apiUrl = 'http://127.0.0.1:5000/api/less_travel_time'; 
+    } else {
+      throw new Error('Invalid criteria');
+    }
+
+    return this.http.post<any>(apiUrl, payload);
+  }
+}
